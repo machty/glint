dea58459901ca5465dd9973bea06266fd4a5000b
machty <machty@gmail.com>
2024-02-07 10:45:21 +0800

    volar 2.0.1

---
+++ test-packages/ts-template-imports-app/src/NoTemplate.gts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+import { TOC } from '@ember/component/template-only';
+
+export class Foo {}
+
---
+++ test-packages/ts-template-imports-app/src/normal.ts
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import { TOC } from '@ember/component/template-only';
+
+export class Foo {}
+
+let b: fo
